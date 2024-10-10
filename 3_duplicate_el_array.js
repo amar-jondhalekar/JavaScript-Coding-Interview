02c19815b2fad9505619e7c6aa0eed6babeb56b9
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-10-10 10:31:38 +0530

    duplicated element

--- 3_duplicate_el_array.js
+++ 3_duplicate_el_array.js
@@ -1,2 +1,9 @@
 // How to find duplicate elements in a given array?
 
+const mainArray = [1, 2, 23, 44, 1, 2, 4, 43, 34, 11, 3, 43];
+const duplicatedArray = mainArray.filter((ele, index, arr) => {
+    return arr.indexOf(ele) !== index
+});
+
+const uniqueDuplicatedArray = [...new Set(duplicatedArray)];
+console.log(`Duplicated Array : `, uniqueDuplicatedArray);
